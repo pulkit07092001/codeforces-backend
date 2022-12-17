edbdd043f110aeaf5d7d9cd7ead990dbc08b1f70
Pulkit <pulkit.garg.eee19@itbhu.ac.in>
2022-12-17 13:51:36 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+mongoose.connect(process.env.MONGO, (err) => {
+  if (err) console.error(err);
+  else console.log("mongoDB connected");
+});
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
